89a7d625d3d7ebe0bd7a7e2c026b9aac011b2d80
Tushar Gautam <109740805+TU7SHAR@users.noreply.github.com>
2026-06-23 17:03:46 +0530

    Remove Bubbl Widget script from layout.js
    Removed Bubbl Widget script from layout.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Suspense } from "react";
 import Loading from "./Loading.jsx";
 import "./globals.css";
@@ -59,9 +58,7 @@ export default function RootLayout({ children }) {
           description="Tushar Guatam - This is a Portfolio Website Explore all my ideas, Reviews Projects , language used and tools used. Get in touch with me for more personalized services opionions and everything "
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <!-- Bubbl Widget -->
-<script src="https://coral-app-jbybo.ondigitalocean.app/static/js/embed.js"
-  data-bot-id="13"></script>
+     
             </head>
       <AuthProvider>
         <body className="flex max-w-[100vw]!important w-[100vw] flex-col bg-[#18181B]">
